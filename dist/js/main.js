092e3409d10e4819d78ed7d0c31243155ac3da70
VamoCodar <adr@Acer.localdomain>
2022-08-08 11:48:56 -0300

    alteracoao do scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -143,11 +143,14 @@
     }
   }
 })({"1SICI":[function(require,module,exports) {
-console.log("oi");
+console.log("/// Seja quem vc quer ser \u2728");
 document.addEventListener("DOMContentLoaded", ()=>{
-    if (window.location.href.endsWith("/admin/")) document.body.classList.add("page-inicial");
+    document.body.classList.add("dashboard");
     console.log(window.location.href.endsWith("/admin/"));
     console.log(window.location.href);
+    if (window.location.href.endsWith("/admin/")) document.body.classList.add("page-inicial");
+    document.body.style.opacity = 1;
+    document.body.classList.add("DOMContentLoaded");
 });
 
 },{}]},["1SICI"], "1SICI", "parcelRequireac12")
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
-console.log("oi");
+
+console.log("/// Seja quem vc quer ser ✨");
+
 
 document.addEventListener("DOMContentLoaded", () => {
+	document.body.classList.add("dashboard")
+
+	console.log(window.location.href.endsWith("/admin/"))
+	console.log(window.location.href)
+	
 	if (window.location.href.endsWith("/admin/")) {
 		document.body.classList.add("page-inicial")
 	}
-	console.log(window.location.href.endsWith("/admin/"))
-	console.log(window.location.href)
+
+	document.body.style.opacity = 1
+	document.body.classList.add("DOMContentLoaded")
+
 })
